c8fe77867f1d554befcc61c48f44d1bebad62fe6
otttera <ott.tera@opilane.tammegymnaasium.ee>
2025-09-18 09:49:45 +0300

    Delete src/kalender/app.py

--- src/kalender/app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
-@app.route('/', methods=['POST', 'GET'])
-def avaleht():
-    return render_template('avaleht.html')
